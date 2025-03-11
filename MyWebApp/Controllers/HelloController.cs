3071ddd8cbf3d59b6806d2908f86174b0acd3f28
Alina <Alina@DESKTOP-1A0OR8I>
2025-03-11 19:35:30 +0300

    Первый коммит

---
+++ MyWebApp/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Привет, мир!";
+    }
+}
